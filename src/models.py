35a2601f6839dd70f1a2c3dfb5b45833ac46584d
Joan Gallego Frances <123939267+joangallego@users.noreply.github.com>
2025-03-19 11:02:34 +0000

    Termiando

--- src/models.py
+++ src/models.py
@@ -1,19 +1,34 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    nombre = db.Column(db.String(80), nullable=False)
+    apellido = db.Column(db.String(80), nullable=False)
+    fecha_suscripcion = db.Column(db.DateTime, nullable=False)
+    favoritos = db.relationship('Favorito', backref='usuario', lazy=True)
 
+class Personaje(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(120), nullable=False)
+    descripcion = db.Column(db.String(250))
+    favoritos = db.relationship('Favorito', backref='personaje', lazy=True)
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+class Planeta(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(120), nullable=False)
+    clima = db.Column(db.String(50))
+    terreno = db.Column(db.String(50))
+    favoritos = db.relationship('Favorito', backref='planeta', lazy=True)
+
+class Favorito(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'), nullable=False)
+    personaje_id = db.Column(db.Integer, db.ForeignKey('personaje.id'), nullable=True)
+    planeta_id = db.Column(db.Integer, db.ForeignKey('planeta.id'), nullable=True)
+
+    def __repr__(self):
+        return f'<Favorito usuario={self.usuario_id} personaje={self.personaje_id} planeta={self.planeta_id}>'
